11452bcce5309bee6ed46a3c18cc712406d99b46
JayJoshiCoditas <jay.joshi@coditas.com>
2019-03-14 20:09:04 +0530

    Trial Commits

--- schoolapp/src/components/HomePage/components/homePage.vue
+++ schoolapp/src/components/HomePage/components/homePage.vue
@@ -7,6 +7,7 @@
             <form class="search-container">
                 <input v-model="defaultDataList" class="inputList" list="schools" name="schools" placeholder="Search Universities" >
                 <datalist id="schools">
+                    Hello World
                     <option value="MIT"></option>
                     <option value="CMU"></option>
                     <option value="Cambridge"></option>
